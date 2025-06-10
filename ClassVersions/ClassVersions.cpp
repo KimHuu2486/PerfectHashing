76180887d61ee7d618fefc631f3b88ebde09510e
HP <HP@LAPTOP-B12DLF2V>
2025-06-10 21:49:45 +0700

    Add project files.

---
+++ ClassVersions/ClassVersions.cpp
@@ -0,0 +1,223 @@
+﻿#include <iostream>
+#include <vector>
+#include <random>
+#include <cmath>
+#include <algorithm>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <unordered_set>
+#include <chrono>
+
+using namespace std;
+
+struct Value {
+    string key;
+    string val;
+};
+
+class PerfectHashTable {
+private:
+    vector<vector<Value>> table;
+    vector<int> setAHash1;
+    vector<vector<int>> setAHash2;
+    vector<bool> hasLevel2Hash;
+    int m = 0;
+    mt19937 gen;
+
+    bool isSpaceOnly(string& s) {
+        return all_of(s.begin(), s.end(), [](char ch) {
+            return isspace(static_cast<unsigned char>(ch));
+            });
+    }
+
+    bool isPrime(int n) {
+        if (n < 2) return false;
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+
+    int findSmallestPrimeGreaterThan(int n) {
+        while (!isPrime(n)) {
+            n++;
+        }
+        return n;
+    }
+
+    int universalHash(const string& key, const vector<int>& setA, int mod) {
+        long long sum = 0;
+        int len = min(key.size(), setA.size());
+        for (int i = 0; i < len; ++i) {
+            int val = static_cast<int>(key[i]);
+            if (val < 0 || val > 127) return -1;
+            sum += val * setA[i];
+        }
+        return sum % mod;
+    }
+
+    void removeDuplicateKeys(vector<Value>& data) {
+        unordered_set<string> seen;
+        vector<Value> filtered;
+        for (const auto& val : data) {
+            if (seen.count(val.key)) continue;
+            seen.insert(val.key);
+            filtered.push_back(val);
+        }
+        data = move(filtered);
+    }
+
+public:
+    PerfectHashTable() : gen(random_device{}()) {}
+
+    void build(const string& path) {
+        vector<Value> data;
+        readFile(path, data);
+        removeDuplicateKeys(data);
+        int n = data.size();
+        m = findSmallestPrimeGreaterThan(n);
+        table.assign(m, vector<Value>());
+
+        // Tạo ngẫu nhiên một tập hợp A1 với độ dài lớn hơn key lớn nhất (dự đoán <100)
+        setAHash1.resize(100);
+        uniform_int_distribution<> dis(0, m - 1);
+        for (int& num : setAHash1) {
+            num = dis(gen);
+        }
+
+        // Hash cấp 1
+        for (const auto& cur : data) {
+            int index = universalHash(cur.key, setAHash1, m);
+            if (index < 0) continue;
+            table[index].push_back(cur);
+        }
+
+        // Đặt cờ kiểm tra việc có xảy ra hàm băm cấp 2 hay không
+        hasLevel2Hash.resize(m, false);
+        // Hash cấp 2
+        setAHash2.resize(m);
+
+        for (int i = 0; i < m; ++i) {
+            int sizeCollision = table[i].size();
+            if (sizeCollision < 2) continue;
+
+            hasLevel2Hash[i] = true;
+            // Bảng băm cấp 2 với độ lớn bằng bình phương số lượng collision tại index thứ ith xảy ra ở hàm băm 1 để tỉ lệ xảy ra collision là tối ưu
+            int sizeTable2 = sizeCollision * sizeCollision * 4;
+            vector<pair<Value, bool>> res(sizeTable2);
+            uniform_int_distribution<> dis2(0, sizeTable2 - 1);
+            bool collision;
+            // Lặp đến khi tạo ra được tập hợp A2 băm các collision tại index ith để không còn xảy ra collision ở hàm băm 2
+            do {
+                collision = false;
+                fill(res.begin(), res.end(), make_pair(Value{ "", "" }, false));
+
+                // Tạo ngẫu nhiên tập hợp A2
+                setAHash2[i].clear();
+                setAHash2[i].resize(100);
+                for (int& num : setAHash2[i]) {
+                    num = dis2(gen);
+                }
+
+                for (const auto& s : table[i]) {
+                    int index2 = universalHash(s.key, setAHash2[i], sizeTable2);
+                    if (res[index2].second) {
+                        collision = true;
+                        break;
+                    }
+                    res[index2] = { s, true };
+                }
+            } while (collision);
+
+            table[i].clear();
+            table[i].resize(sizeTable2);
+            for (int j = 0; j < sizeTable2; j++) {
+                table[i][j] = res[j].first;
+            }
+        }
+    }
+
+    void readFile(const string& path, vector<Value>& vec) {
+        ifstream ifs(path);
+        if (!ifs.is_open()) {
+            cerr << "Can't open file: " << path << endl;
+            return;
+        }
+        string temp;
+        Value cur;
+        while (getline(ifs, temp)) {
+            if (temp.empty() || isSpaceOnly(temp) || temp.size() < 3) continue;
+            stringstream ss(temp);
+            ss >> cur.key;
+            cur.val = temp;
+            vec.push_back(cur);
+        }
+        ifs.close();
+    }
+
+    string search(const string& key) {
+        int index1 = universalHash(key, setAHash1, table.size());
+
+        if (index1 >= table.size() || table[index1].empty()) {
+            return "Not found";
+        }
+
+        if (!hasLevel2Hash[index1]) {
+            // Không có băm cấp 2, kiểm tra key
+            if (table[index1].begin()->key == key) {
+                return table[index1].begin()->val;
+            }
+            return "Not found";
+        }
+
+        // Có dùng băm cấp 2
+        int index2 = universalHash(key, setAHash2[index1], table[index1].size());
+        if (index2 >= table[index1].size() || table[index1][index2].key != key) {
+            return "Not found";
+        }
+
+        return table[index1][index2].val;
+    }
+
+};
+
+int main() {
+    string path;
+    cout << "Input the path of file: ";
+    cin >> path;
+
+    PerfectHashTable pht;
+    auto start = chrono::high_resolution_clock::now();
+    pht.build(path);
+    auto end = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<chrono::milliseconds>(end - start);
+    cout << "Time for hashing data: " << duration.count() << "ms" << endl << endl;
+
+    int n;
+    cout << "Input the numbers of words which you want to search: ";
+    cin >> n;
+    vector<string> vec(n);
+    cout << "Input " << n << " words: " << endl;
+    for (string& s : vec) {
+        cin >> s;
+    }
+
+    const int repeat = 1000000; // lặp lại 1 triệu lần
+
+    for (const string& s : vec) {
+        start = chrono::high_resolution_clock::now();
+        string temp;
+        for (int i = 0; i < repeat; ++i) {
+            temp = pht.search(s);
+        }
+        end = chrono::high_resolution_clock::now();
+
+        auto duration_ns = chrono::duration_cast<chrono::nanoseconds>(end - start);
+        long long duration_ps = duration_ns.count() * 1000; // nanoseconds -> picoseconds
+        cout << "Result: " << pht.search(s) << endl;
+        cout << "Average time search (per operation): " << duration_ps / repeat << " ps" << endl;
+    }
+
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,58 @@
+﻿# Perfect Hashing
+
+## Mô tả
+Dự án này triển khai **Perfect Hashing** sử dụng kĩ thuật **băm hai cấp** để đảm bảo **không xảy ra va chạm** (Collision). Mục tiêu là để tối ưu hóa tốc độ tìm kiếm dữ liệu trong tập dữ liệu lớn với thời gian là hằng số **O(1)**.
+
+## Tính năng
+- Đọc dữ liệu từ file văn bản.
+- Loại bỏ các khóa trùng lặp.
+- Sử dụng hàm băm phổ quát (universal hashing).
+- Băm hai cấp để tránh va chạm.
+- Tìm kiếm từ khóa với thời gian là hằng số.
+- Đo thời gian xây dựng bảng băm và thời gian tìm kiếm.
+
+## Cài đặt
+
+### Yêu cầu:
+- Visual Studio 2022 hoặc trình biên dịch C++ hỗ trợ C++17 trở lên.
+
+### Cách chạy:
+1. Mở file `main.cpp` trong Visual Studio 2022 hoặc trình biên dịch có hỗ trợ C++.
+2. Đảm bảo file dữ liệu đầu vào (ví dụ: `Original_1.txt`) có định dạng mỗi đoạn (không xuống dòng) là một chuỗi, trong đó từ khóa là từ đầu tiên.
+3. Biên dịch và chạy chương trình.
+4. Nhập đường dẫn đến file dữ liệu khi được yêu cầu.
+5. Nhập số lượng từ khóa và từ khóa cần tra cứu khi được yêu cầu.
+
+
+## Cấu trúc file
+
+- `main.cpp`: Chứa toàn bộ mã nguồn của chương trình.
+- `Original_1.txt`, `Original_2.txt`, `Original_3.txt`: File dữ liệu đầu vào (do người dùng cung cấp).
+- `README.md`: Tài liệu mô tả dự án.
+
+
+## Ví dụ dữ liệu
+
+```txt
+Apple  n. 1 roundish firm fruit with crisp flesh. 2 tree bearing this.  apple of one's eye cherished person or thing. [old english]
+Banana  n. 1 long curved soft fruit with a yellow skin. 2 treelike plant bearing it.  go bananas slang go mad. [portuguese or spanish, from an african name]
+Bike  colloq. —n. Bicycle or motor cycle. —v. (-king) ride a bike.  biker n. [abbreviation]
+Bronze medal  n. Medal, usu. Awarded as third prize.
+Dirty  —adj. (-ier, -iest) 1 soiled, unclean. 2 causing dirtiness (dirty job).
+```
+
+## Ví dụ cách chạy
+```txt
+Input the path of file: Original_1.txt
+Time for hashing data: 142ms
+
+Input the numbers of words which you want to search: 2
+Input 2 words:
+Apple
+Bronze
+Result: Apple  n. 1 roundish firm fruit with crisp flesh. 2 tree bearing this.  apple of one's eye cherished person or thing. [old english]
+Average time search (per operation): 147 ps
+Result: Bronze  ùn. 1 alloy of copper and tin. 2 its brownish colour. 3 thing of bronze, esp. A sculpture. ùadj. Made of or coloured like bronze. ùv. (-zing) make or become brown; tan. [french from italian]
+Average time search (per operation): 151 ps
+```
+
